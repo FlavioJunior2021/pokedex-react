03ec227c574f3d23846a5a2a9cf495d4e56e198a
Flávio Oliveira Silva Júnior <flavio.junior@escolar.ifrn.edu.br>
2021-12-13 09:02:40 -0300

    Update App

--- src/components/SearchComponent/index.jsx
+++ src/components/SearchComponent/index.jsx
@@ -7,17 +7,7 @@ import './index.scss';
 
 export function SearchComponent(){
 
-    const [pokeImg, setPokeImg] = useState("");
-    const [name, setName] = useState("");
-    const [hp, setHp] = useState(0);
-    const [attack, setAttack] = useState(0);
-    const [defense, setDefense] = useState(0);
-    const [defenseMax, setDefenseMax] = useState(0);
-    const [attackMax, setAttackMax] = useState(0);
-    const [speed, setSpeed] = useState(0);
-    const [type, setType] = useState("");
-    const [weight, setWeight] = useState(0);
-    const [species, setSpecies] = useState("");
+    const [pokeObj, setPokeObj] = useState({})
 
     function getValue(e){
         if(e.key === "Enter"){
@@ -27,19 +17,8 @@ export function SearchComponent(){
     }
     async function handleSearch(searchThis){
         let search = await fetchPokemon(searchThis);
-        setPokeImg(search.sprite);
-        setName(search.name);
-        setHp(search.hp);
-        setAttack(search.attack);
-        setDefense(search.defense);
-        setDefenseMax(search.especial2);
-        setAttackMax(search.especial);
-        setSpeed(search.speed);
-        setType(search.type);
-        setWeight(search.weight);
-        setSpecies(search.species);
+        setPokeObj(search);
     }
-
     return(
         <div id="search-component">
             <div className="search-container">
@@ -47,9 +26,9 @@ export function SearchComponent(){
                     onKeyDown={(e) => getValue(e)}
                 />
             </div>
-            <ShowThisComponent 
-                image={ pokeImg }
-            />
+           <ShowThisComponent 
+            image={ pokeObj.sprite }
+           />
         </div>
     );
 };
--- src/components/ShowThisComponent/index.jsx
+++ src/components/ShowThisComponent/index.jsx
@@ -1,10 +1,12 @@
-import { useState , useEffect } from 'react';
 import './index.scss';
 
 export function ShowThisComponent( { name,image,hp,attack,defense,special,special2,speed,type,weight,species } ){
 
     return(
         <div id="show-component">
+            <div className="poke-name">
+                <h2>{name}</h2>
+            </div>
             <div className="poke-ilustration">
                 <img src={image} alt="" />
             </div>

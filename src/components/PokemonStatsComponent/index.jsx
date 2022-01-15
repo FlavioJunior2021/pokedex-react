6a8926d8377500e86b6130854eba6bf0c499b355
Flávio Oliveira Silva Júnior <flavio.junior@escolar.ifrn.edu.br>
2022-01-15 12:04:21 -0300

    Update App

---
+++ src/components/PokemonStatsComponent/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.scss';
+
+export function PokemonStats({life, attack, defense, especialAttactk, especialDefense, speed}){
+    return(
+        <div className="pokemon-stats">
+            <section id="life" style={{border: "1px solid black", backgroundColor: 'green'}}>
+                    
+            </section>
+        </div>
+    );
+};
--- src/components/ShowThisComponent/index.jsx
+++ src/components/ShowThisComponent/index.jsx
@@ -42,45 +42,8 @@ export function ShowThisComponent( { name,image,hp,attack,defense,especial,espec
                 </div>
             </div>
             <h1>Statistics</h1>
-            <div className="info-stats">
-                <p style={{backgroundColor: 'green'}}>life</p>
-                <p style={{backgroundColor: '#FFCE2B'}}>speed</p>
-                <p style={{backgroundColor: 'red'}}>attack</p>
-                <p style={{backgroundColor: 'blue'}}>defense</p>
-                <p style={{backgroundColor: '#371075'}}>especial defense</p>
-                <p style={{backgroundColor: '#F25C05'}}>especial attack</p>
-            </div>
             <div className="pokemon-stats">
-                <div id="life" style={{border: "1px solid black", backgroundColor: 'green'}}>
-                    <h3>
-                        { hp }
-                    </h3>
-                </div>
-                <div id="speed" style={{border: "1px solid black", backgroundColor: '#FFCE2B'}}>
-                    <h3>
-                        { speed }
-                    </h3>
-                </div>
-                <div id="attack" style={{border: "1px solid black", backgroundColor: 'red'}}>
-                    <h3>
-                        { attack }
-                    </h3>
-                </div>
-                <div id="defense" style={{border: "1px solid black", backgroundColor: 'blue'}}>
-                    <h3>
-                        { defense }
-                    </h3>
-                </div>
-                <div id="especial-defense" style={{border: "1px solid black", backgroundColor: '#371075'}}>
-                    <h3>
-                        { especial2 }
-                    </h3>
-                </div>
-                <div id="especial-attack" style={{border: "1px solid black", backgroundColor: '#F25C05'}}>
-                    <h3>
-                        { especial }
-                    </h3>
-                </div>
+                
             </div>
         </div>
     );

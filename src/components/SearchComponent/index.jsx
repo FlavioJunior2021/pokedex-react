489176462c8fadccd39e94019a2f93ecfd1b853b
Flávio Oliveira Silva Júnior <flavio.junior@escolar.ifrn.edu.br>
2022-01-16 12:01:32 -0300

    Update index.jsx

--- src/components/SearchComponent/index.jsx
+++ src/components/SearchComponent/index.jsx
@@ -35,7 +35,7 @@ export function SearchComponent(){
         <div id="search-component">
             <Toaster
                 toastOptions={{
-                    duration: 4000,
+                    duration: 3000,
                     style:{
                         border: '1px solid #000',
                         padding: '16px',
